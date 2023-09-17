0d2c5b1ca0d3772420af146e23b8d6820d49147f
stella <slysly129129@163.com>
2023-09-17 20:04:14 +0800

    Stella 9.16 commit

--- src/main/java/com/login/control/LoginServlet.java
+++ src/main/java/com/login/control/LoginServlet.java
@@ -2,9 +2,11 @@ package com.login.control;
 
 import com.login.entity.Users;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", value = "/LoginServlet")
