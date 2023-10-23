233ac183d8d8daf2db2861c7bda84d5d3c9f9cd3
stella <slysly129129@163.com>
2023-10-23 15:29:23 +0800

    Stella 10.23 commit

---
+++ src/main/java/com/cookie/CookieServlet.java
@@ -0,0 +1,30 @@
+package com.cookie;
+
+import com.login.control.HelloServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CookieServlet extends HelloServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+
+        //创建cookie对象
+        Cookie cookie = new Cookie("001", "Hongkong");
+        //将cookie设置到response对象中
+        response.addCookie(cookie);
+        //给cookie对象添加有效期，单位是秒
+        cookie.setMaxAge(3*24*3600);
+        response.getWriter().write("studying cookie");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
